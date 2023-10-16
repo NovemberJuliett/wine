0ced1dbbf5b7b1990d3076e9ea1235d9b2187a62
NovemberJuliett <137638315+NovemberJuliett@users.noreply.github.com>
2023-10-16 17:33:01 +0300

    update main.py

--- main.py
+++ main.py
@@ -11,18 +11,17 @@ dict_list = list(wine_dictionary.values())
 internal_dict = dict_list[0]
 wines_list = []
 for wine in internal_dict:
-    new_dict = {}
+    result = {}
     for key in wine_dictionary:
         dict_value = wine_dictionary[key]
         null_value = dict_value[wine]
-        new_dict[key] = null_value
-    wines_list.append(new_dict)
+        result[key] = null_value
+    wines_list.append(result)
 # print(wines_list)
 
 
 second_excel_file = pandas.read_excel('wine2.xlsx')
 second_file_dict = second_excel_file.to_dict()
-print(second_file_dict)
 second_file_keys = list(second_file_dict.keys())
 second_file_values = list(second_file_dict.values())
 # print(second_file_keys)
@@ -30,20 +29,16 @@ second_file_values = list(second_file_dict.values())
 
 category_values = second_file_values[0]
 print(category_values)
-new_dict = {}
+result = {}
 for key in category_values:
-    value = category_values[key]
-    item_numbers = new_dict.get(value)
+    category = category_values[key]
+    item_numbers = result.get(category)
     if item_numbers is not None:
         item_numbers.append(key)
     else:
-        new_dict[value] = [key]
-print(new_dict)
-zzz_key = new_dict.get("zzz")
-print(zzz_key)
-drinks = new_dict.get('Напитки')
-print(drinks)
-print(type(drinks))
+        result[category] = [key]
+print(result)
+
 
 
 env = Environment(
