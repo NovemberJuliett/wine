f4f69b7c9fb71ef51485f6eeb556b783e65849a6
NovemberJuliett <137638315+NovemberJuliett@users.noreply.github.com>
2023-10-20 18:48:17 +0300

    update main.py and template.html

--- main.py
+++ main.py
@@ -5,30 +5,15 @@ import pandas
 from pprint import pprint
 import collections
 
-# first_excel_file = pandas.read_excel('wine.xlsx')
-# wine_dictionary = first_excel_file.to_dict()
-#
-#
-# dict_list = list(wine_dictionary.values())
-# internal_dict = dict_list[0]
-# wines_list = []
-# for wine in internal_dict:
-#     result = {}
-#     for key in wine_dictionary:
-#         dict_value = wine_dictionary[key]
-#         null_value = dict_value[wine]
-#         result[key] = null_value
-#     wines_list.append(result)
 
-
-second_excel_file = pandas.read_excel('wine2.xlsx', keep_default_na=False)
-second_file_dict = second_excel_file.to_dict(orient='records')
+excel_file = pandas.read_excel('wine3.xlsx', keep_default_na=False)
+excel_file_dict = excel_file.to_dict(orient='records')
 
 category_dict = collections.defaultdict(list)
-for key in second_file_dict:
+for key in excel_file_dict:
     category = key['Категория']
     category_dict[category].append(key)
-print(category_dict)
+pprint(category_dict)
 
 
 env = Environment(
